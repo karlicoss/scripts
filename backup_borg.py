793fd083631ec915193c0f0bbdf05e030b33306c
Dima Gerasimov <karlicoss@gmail.com>
2019-01-30 22:54:03 +0000

    reuse common bits for scripts

---
+++ backup_borg.py
@@ -0,0 +1,45 @@
+from os.path import lexists
+from subprocess import check_output, run, check_call
+
+def do_borg(repo, paths):
+    def borg(*args):
+        run(
+            ['borg', *args],
+            check=True,
+            env={
+                'BORG_REPO': repo,
+            },
+        )
+
+    if not lexists(repo):
+        borg('init', '--encryption=none')
+
+    # TODO how to keep options in sync?
+    borg(
+        'create',
+        '--compression', 'lz4', # fast, not super efficient
+        '--verbose',
+        '--filter', 'AME', # only list changes, exclude present files
+        '--list',
+        '--stats',
+        '--show-rc', # return code
+
+
+        '--exclude-caches',
+        '--exclude', '**/.dropbox.cache',
+
+        # python
+        '--exclude', '**/.mypy_cache',
+        '--exclude', '**/.tox',
+
+        '--exclude', '**/node_modules',
+
+        '--exclude', '**/.stack-work', # haskell
+
+        '--exclude-if-present', '.rustc_info.json', # rust
+        '--exclude-if-present', '.borg-exclude', # custom
+        "::{hostname}-{utcnow}",
+        *paths,
+    )
+
+    # TODO pruning? 
