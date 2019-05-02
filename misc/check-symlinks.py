e612c5a9e2678743b6fc66eb3bdddeb36d2c967c
Dima Gerasimov <karlicoss@gmail.com>
2019-05-02 21:07:35 +0100

    use kython module

--- misc/check-symlinks.py
+++ misc/check-symlinks.py
@@ -3,28 +3,11 @@ import os
 from os.path import isdir, islink, exists, isfile, join, basename
 from pathlib import Path
 
-from enum import Enum
 import sys
 
 import logging
 from kython import setup_logzero
-from kython import PathIsh
-from kython.fs import apply_on
-
-
-class XX(Enum):
-    BAIL = 'bail'
-    REC  = 'recurse'
-
-
-def traverse(root: PathIsh, handler, logger=None):
-    if logger is None:
-        logger = logging.getLogger('fs-traverse')
-    for root, dirs, files in os.walk(root):
-        res = handle(root, dirs, files) # TODO map to Path?
-        if res == XX.BAIL:
-            logger.info('skipping %s', root) # TODO reason would be nice?
-            dirs[:] = []
+from kython.fs import Go, traverse
 
 
 def get_logger():
@@ -38,18 +21,18 @@ IGNORED_DIRS = [
 # TODO eh, global variable...
 broken = []
 
-def handle(root, dirs, files) -> XX:
+def handle(root, dirs, files) -> Go:
     logger = get_logger()
     if basename(root) in IGNORED_DIRS:
-        return XX.BAIL
+        return Go.BAIL
     if '.symlink-checker-ignore' in files:
-        return XX.BAIL
+        return Go.BAIL
     for f in files:
         ff = join(root, f)
         if islink(ff) and not exists(ff):
             broken.append(ff)
             logger.error(f"broken: {ff}")
-    return XX.REC
+    return Go.REC
 
 
 def main():
