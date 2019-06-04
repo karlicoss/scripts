1d81ea9b250537380abffdd0e7a4256053040afa
Dima Gerasimov <karlicoss@gmail.com>
2019-06-04 22:31:46 +0100

    spacemacs script

--- emacs/update-spacemacs.sh
+++ emacs/update-spacemacs.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eux
+
+
+# check no emacs instances are running
+! pgrep -a emacs || exit 1
+
 cd ~/.emacs.d
-git pull -p
-rm -rf .cache
 rm -rf elpa
+
+rm -rf .cache
+mkdir .cache
+ln -s ~/configs-nogit/emacs/places  .cache/places
+ln -s ~/configs-nogit/emacs/recentf .cache/recentf
+
+git pull -p
