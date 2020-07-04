5e05a74723108b2e1752801db75aadfccf355da5
Dima Gerasimov <karlicoss@gmail.com>
2020-07-04 22:02:42 +0100

    rely on common excludes??

--- borg/backup_home.py
+++ borg/backup_home.py
@@ -3,44 +3,56 @@ from pathlib import Path
 
 from backup_borg import do_borg
 
+# todo not sure if should autodetect..
+USER = 'karlicos'
+
+
 def do_borg_home(**kwargs):
+    REPO = str(Path(__file__).absolute().parent / 'home')
     return do_borg(
-        repo=str(Path(__file__).absolute().parent / 'home'),
-        paths=['/home/karlicos'],
-        exclude=['/home/karlicos/' + p for p in [
-            '.borg-passphrase',
+        repo=REPO,
+        paths=[f'/home/{USER}'],
+        # TODO reuse excludefrom file?
+        # exclude=[f'/home/{USER}/' + p for p in [
+        #     '.borg-passphrase',
 
-            '.cache/borg',
-            '.cache/chromium',
-            '.cache/go-build',
-            '.cache/jedi',
-            '.cache/mozilla',
-            '.cache/pip',
-           
-            '.config/syncthing/index-*',
+        #     '.cache/borg',
+        #     '.cache/chromium',
+        #     '.cache/go-build',
+        #     '.cache/jedi',
+        #     '.cache/mozilla',
+        #     '.cache/pip',
+        #    
+        #     TODO ugh. yet another argument for keeping it elsewhere...
+        #     '.config/syncthing/index-*',
 
-            '.recoll/xapiandb',
+        #     '.recoll/xapiandb',
 
 
-            '.dropbox',
-            '.dropbox-dist*',
+        #     '.dropbox',
+        #     '.dropbox-dist*',
 
-            '.cabal',
-            '.cargo',
-            '.gradle',
-            '.npm',
-            '.rustup',
-            '.stack',
+        #     '.cabal',
+        #     '.cargo',
+        #     '.gradle',
+        #     '.npm',
+        #     '.rustup',
+        #     '.stack',
 
-            '.local/lib',
-            '.local/share/Trash',
+        #     '.local/lib',
+        #     '.local/share/Trash',
 
-            'snap/spotify',
+        #     'snap/spotify',
  
-            ## glumov, perm errors
-            '.rnd',
-            'postgres',
-            ##
-        ]], 
+        #     ## glumov, perm errors
+        #     '.rnd',
+        #     'postgres',
+        #     ##
+        # ]], 
         **kwargs,
     )
+# TODO prune
+
+if __name__ == '__main__':
+    do_borg_home()
+
